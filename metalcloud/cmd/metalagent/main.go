fb1326eec11a6ebb5ab4d19698da56ff9239d8a4
justinsb <justinsb@google.com>
2023-11-19 11:07:06 -0500

    Load modules using modprobe

--- metalcloud/cmd/metalagent/main.go
+++ metalcloud/cmd/metalagent/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 
 	agentv1 "github.com/justinsb/metalcloud/agent/v1"
@@ -67,6 +70,19 @@ func mountDev() error {
 	return nil
 }
 
+func mountSys() error {
+	dir := "/sys"
+
+	if err := os.MkdirAll(dir, 0555); err != nil {
+		return fmt.Errorf("failed to MkdirAll(%q): %w", dir, err)
+	}
+
+	if err := unix.Mount("sysfs", dir, "sysfs", 0, ""); err != nil {
+		return fmt.Errorf("failed to mount %s: %w", dir, err)
+	}
+	return nil
+}
+
 func mountProc() error {
 	dir := "/proc"
 
@@ -91,16 +107,95 @@ func poll(ctx context.Context) error {
 		return fmt.Errorf("failed to MkdirAll(%q): %w", "/etc", err)
 	}
 
-	if err := os.MkdirAll("proc", 0755); err != nil {
-		return fmt.Errorf("failed to MkdirAll(%q): %w", "/proc", err)
-	}
-
-	if err := unix.Mount("proc", "/proc", "proc", 0, ""); err != nil {
-		return fmt.Errorf("failed to mount /proc: %w", err)
-	}
-
-	if err := runCommand(ctx, "/bin/busybox", "--install", "-s", "/bin"); err != nil {
+	kernelVersion, err := kmod.UName()
+	if err != nil {
 		return err
+	}
+
+	{
+		devicesDir := "/sys/devices"
+
+		var modaliases []string
+
+		if err := filepath.WalkDir(devicesDir, func(path string, d os.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			name := filepath.Base(path)
+			if name == "modalias" {
+				b, err := os.ReadFile(path)
+				if err != nil {
+					return fmt.Errorf("error reading %q: %w", path, err)
+				}
+				modaliases = append(modaliases, string(b))
+				log.Printf("modalias %v is %v", path, string(b))
+			}
+			return nil
+		}); err != nil {
+			return fmt.Errorf("error walking %q: %w", devicesDir, err)
+		}
+
+		log.Printf("modaliases is %v", modaliases)
+
+		loadModules := make(map[string]bool)
+		{
+
+			// TODO: load modules.aliases once
+
+			aliases := filepath.Join("/lib", "modules", kernelVersion, "modules.alias")
+			f, err := os.Open(aliases)
+			if err != nil {
+				return fmt.Errorf("error opening %q: %w", aliases, err)
+			}
+			defer f.Close()
+
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				line := scanner.Text()
+				if strings.HasPrefix(line, "#") {
+					continue
+				}
+
+				tokens := strings.Fields(line)
+				if len(tokens) != 3 {
+					log.Printf("unexpected line in %q: %q", aliases, line)
+					continue
+				}
+
+				for _, modalias := range modaliases {
+					if matched, _ := filepath.Match(tokens[1], modalias); matched {
+						module := tokens[2]
+						log.Printf("found module %q for %q", module, modalias)
+						loadModules[module] = true
+					}
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				return fmt.Errorf("error reading %q: %w", aliases, err)
+			}
+		}
+
+		var moduleNames []string
+		for module := range loadModules {
+			moduleNames = append(moduleNames, module)
+		}
+		dep, err := kmod.LoadModulesDep()
+		if err != nil {
+			return err
+		}
+		log.Printf("loading modules %v", moduleNames)
+		if err := kmod.LoadModules(moduleNames, dep); err != nil {
+			log.Printf("failed to load modules: %v", err)
+		}
+	}
+
+	if _, err := os.Stat("/bin/ip"); err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("error getting stat on /bin/ip: %w", err)
+		}
+		if err := runCommand(ctx, "/bin/busybox", "--install", "-s", "/bin"); err != nil {
+			return err
+		}
 	}
 
 	if err := runIP(ctx, "link"); err != nil {
@@ -111,8 +206,19 @@ func poll(ctx context.Context) error {
 		return err
 	}
 
-	if err := runIP(ctx, "link", "set", "dev", "eth0", "up"); err != nil {
-		return err
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return fmt.Errorf("error getting interfaces: %w", err)
+	}
+	for _, iface := range interfaces {
+		if (iface.Flags & net.FlagUp) == 0 {
+			log.Printf("setting iface %q to up", iface.Name)
+			if err := runIP(ctx, "link", "set", "dev", iface.Name, "up"); err != nil {
+				return err
+			}
+		} else {
+			log.Printf("iface %q is up", iface.Name)
+		}
 	}
 
 	if err := runDHCP(ctx); err != nil {
@@ -155,35 +261,44 @@ func runDHCP(ctx context.Context) error {
 }
 
 func bootstrapMachine(ctx context.Context) error {
-	modules := []string{
-		// USB keyboards
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/usb/common/usb-common.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/usb/core/usbcore.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/hid/hid.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/hid/hid-generic.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/hid/usbhid/usbhid.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/input/evdev.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/usb/host/xhci-hcd.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/usb/host/xhci-pci.ko",
+	// kernelVersion, err := kmod.UName()
+	// if err != nil {
+	// 	return err
+	// }
 
-		// 8169
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/net/phy/libphy.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/net/phy/realtek.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/net/phy/mdio_devres.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/net/ethernet/realtek/r8169.ko",
+	// modules := []string{
+	// 	// USB keyboards
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/usb/common/usb-common.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/usb/core/usbcore.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/hid/hid.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/hid/hid-generic.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/hid/usbhid/usbhid.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/input/evdev.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/usb/host/xhci-hcd.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/usb/host/xhci-pci.ko",
 
-		// 8139
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/net/mii.ko",
-		"/lib/modules/5.10.0-18-amd64/kernel/drivers/net/ethernet/realtek/8139cp.ko",
-	}
-	for _, module := range modules {
-		if err := kmod.LoadModule(module, "", 0); err != nil {
-			return err
-		}
-		log.Printf("loaded module %q\n", module)
-	}
+	// 	// 8169
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/net/phy/libphy.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/net/phy/realtek.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/net/phy/mdio_devres.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/net/ethernet/realtek/r8169.ko",
+
+	// 	// 8139
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/net/mii.ko",
+	// 	"/lib/modules/" + kernelVersion + "/kernel/drivers/net/ethernet/realtek/8139cp.ko",
+	// }
+	// for _, module := range modules {
+	// 	if err := kmod.LoadModuleByPath(module, "", 0); err != nil {
+	// 		return err
+	// 	}
+	// 	log.Printf("loaded module %q\n", module)
+	// }
 
 	if err := mountDev(); err != nil {
+		return err
+	}
+
+	if err := mountSys(); err != nil {
 		return err
 	}
 
--- metalcloud/pkg/kmod/modprobe.go
+++ metalcloud/pkg/kmod/modprobe.go
@@ -1,13 +1,37 @@
 package kmod
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
+	"syscall"
 
 	"golang.org/x/sys/unix"
 )
 
-func LoadModule(modulePath string, params string, flags int) error {
+func UName() (string, error) {
+	u := syscall.Utsname{}
+	if err := syscall.Uname(&u); err != nil {
+		return "", fmt.Errorf("error from uname: %w", err)
+	}
+
+	var bytes []byte
+	for i := 0; ; i++ {
+		if u.Release[i] == 0 {
+			break
+		}
+		bytes = append(bytes, byte(u.Release[i]))
+	}
+	kernelVersion := string(bytes)
+	log.Printf("kernel version is %v", kernelVersion)
+	return kernelVersion, nil
+}
+
+func LoadModuleByPath(modulePath string, params string, flags int) error {
+	log.Printf("loading module %q", modulePath)
 	f, err := os.Open(modulePath)
 	if err != nil {
 		return fmt.Errorf("error opening file %q: %w", modulePath, err)
@@ -15,7 +39,208 @@ func LoadModule(modulePath string, params string, flags int) error {
 	defer f.Close()
 
 	if err := unix.FinitModule(int(f.Fd()), params, flags); err != nil {
+		if errno, ok := err.(syscall.Errno); ok {
+			switch errno {
+			case syscall.EEXIST:
+				log.Printf("ignoring EEXIST from finit(%q)", modulePath)
+				return nil
+			}
+		}
 		return fmt.Errorf("error from finit(%q, %q, %v): %w", modulePath, params, flags, err)
 	}
 	return nil
+}
+
+type ModulesDep struct {
+	baseDir string
+	modules map[string]*moduleInfo
+}
+
+type moduleInfo struct {
+	path         string
+	dependencies []string
+}
+
+func LoadModulesDep() (*ModulesDep, error) {
+	kernelVersion, err := UName()
+	if err != nil {
+		return nil, err
+	}
+
+	modules := make(map[string]*moduleInfo)
+
+	baseDir := filepath.Join("/lib", "modules", kernelVersion)
+	p := filepath.Join(baseDir, "modules.dep")
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, fmt.Errorf("error opening %q: %w", p, err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		tokens := strings.Fields(line)
+		if !strings.HasSuffix(tokens[0], ":") {
+			log.Printf("unexpected line (no colon) in %q: %q", p, line)
+			continue
+		}
+
+		modulePath := strings.TrimSuffix(tokens[0], ":")
+		moduleName := filepath.Base(modulePath)
+		moduleName = strings.TrimSuffix(moduleName, ".ko")
+
+		info := &moduleInfo{
+			path: modulePath,
+		}
+		for _, dep := range tokens[1:] {
+			name := filepath.Base(dep)
+			name = strings.TrimSuffix(name, ".ko")
+
+			info.dependencies = append(info.dependencies, name)
+		}
+		modules[moduleName] = info
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading %q: %w", p, err)
+	}
+
+	return &ModulesDep{baseDir: baseDir, modules: modules}, nil
+}
+
+func buildLoadedModules() (map[string]bool, error) {
+	p := filepath.Join("/proc", "modules")
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, fmt.Errorf("error opening %q: %w", p, err)
+	}
+	defer f.Close()
+
+	modules := make(map[string]bool)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		tokens := strings.Fields(line)
+		moduleName := tokens[0]
+		log.Printf("module %q already loaded", moduleName)
+		modules[moduleName] = true
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading %q: %w", p, err)
+	}
+
+	return modules, nil
+}
+
+type moduleLoadPlan struct {
+	deps *ModulesDep
+
+	done map[string]bool
+
+	queue map[string]*moduleInfo
+}
+
+func (p *moduleLoadPlan) addToPlan(moduleName string) error {
+	if p.done[moduleName] {
+		// Already loaded
+		return nil
+	}
+
+	modInfo, found := p.deps.modules[moduleName]
+	if !found {
+		log.Printf("module %q not known", moduleName)
+		return fmt.Errorf("module %q not known", moduleName)
+	}
+
+	if _, found := p.queue[moduleName]; found {
+		// Already in queue
+		return nil
+	}
+
+	for _, dep := range modInfo.dependencies {
+		// We can't load a dependency, so we can't load this one
+		if err := p.addToPlan(dep); err != nil {
+			return err
+		}
+	}
+
+	p.queue[moduleName] = modInfo
+
+	return nil
+}
+
+func LoadModules(moduleNames []string, deps *ModulesDep) error {
+	plan := &moduleLoadPlan{
+		deps:  deps,
+		done:  make(map[string]bool),
+		queue: make(map[string]*moduleInfo),
+	}
+
+	loadedModules, err := buildLoadedModules()
+	if err != nil {
+		return err
+	}
+	for module := range loadedModules {
+		plan.done[module] = true
+	}
+
+	for _, moduleName := range moduleNames {
+		if err := plan.addToPlan(moduleName); err != nil {
+			log.Printf("will not be able to load module %q: %v", moduleName, err)
+		}
+	}
+
+	for {
+		progress := false
+		allDone := true
+
+		for moduleName, info := range plan.queue {
+			if plan.done[moduleName] {
+				continue
+			}
+
+			allDone = false
+
+			dependenciesLoaded := true
+			for _, dep := range info.dependencies {
+				if !plan.done[dep] {
+					log.Printf("cannot yet load %q, waiting on %q", moduleName, dep)
+					dependenciesLoaded = false
+				}
+			}
+
+			if !dependenciesLoaded {
+				continue
+			}
+
+			fullPath := filepath.Join(deps.baseDir, info.path)
+			if err := LoadModuleByPath(fullPath, "", 0); err != nil {
+				log.Printf("error loading %q", fullPath)
+				log.Printf("  error is %v", err)
+				//return fmt.Errorf("error loading %q: %w", fullPath, err)
+				continue
+			}
+			log.Printf("loaded module %q", info.path)
+			plan.done[moduleName] = true
+			progress = true
+		}
+
+		if allDone {
+			return nil
+		}
+
+		if !progress {
+			return fmt.Errorf("not making progress loading modules")
+		}
+	}
+
 }
